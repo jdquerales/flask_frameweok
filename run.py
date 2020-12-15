f2f1616d75addf944b48f44285d3ea0bd515c9cf
jdquerales <jquerales72@gmail.com>
2020-12-15 11:17:43 +0000

    Add flash functionality in contact form

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
 
 @app.route("/")
 def index():
@@ -12,13 +16,27 @@ def index():
 @app.route("/about")
 def about():
     data = []
-    with open("data/company.json", 'r') as json_data:
+    with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -29,7 +47,6 @@ def careers():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP","0.0.0.0"),
+        host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True
-    )
+        debug=True)
